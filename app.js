bdad12a1bd5f390b40111af3d56a9025ca66d134
Gowtham Kumar Rodda <gowtham@olivetech.net>
2018-09-06 23:58:17 -0500

    refactored app.js

--- app.js
+++ app.js
@@ -12,117 +12,103 @@ const debug = require('debug')('sentimentsl-analysis:server');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+class Server {
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  constructor(app, http) {
+    this.app = app;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    // view engine setup
+    this.app.set('views', path.join(__dirname, 'views'));
+    this.app.set('view engine', 'ejs');
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({extended: false}));
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, 'public')));
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    this.app.use('/', indexRouter);
+    this.app.use('/users', usersRouter);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // cthis.atch 404 and forward to error handler
+    this.app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // ethis.rror handler
+    this.app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    this.port = this.normalizePort(process.env.PORT || '3000');
+    this.app.set('port', this.port);
 
 
-/**
- * Get port from environment and store in Express.
- */
-let port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+    this.server = http.createServer(this.app);
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+    this.server.listen(this.port);
+    this.server.on('error', this.onError);
+    this.server.on('listening', this.onListening.bind(this));
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
   }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
+  onError(error) {
+    if (error.syscall !== 'listen') {
       throw error;
+    }
+
+    const bind = typeof this.port === 'string'
+      ? 'Pipe ' + this.port
+      : 'Port ' + this.port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
   }
+
+  onListening() {
+    const addr = this.server.address();
+    const bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+    console.log('-------------> server listening on', bind);
+  }
+
+  normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+
+    return false;
+  }
+
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-async function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-
-  console.log('Connecting to MongoDB ...'.red);
-  Mongo.connect();
-}
+new Server(express(), http);
 
